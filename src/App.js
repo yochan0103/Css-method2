a0f1a8f61b990e4d6d5bbab40509f32f168f13d2
yochan0103 <68014172+yochan0103@users.noreply.github.com>
2021-11-14 13:42:16 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import { InlineStyle } from "./componets/InlineStyle";
 import { CssModules } from "./componets/CssModules";
+import { StyledJsx } from "./componets/StyledJsx";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
